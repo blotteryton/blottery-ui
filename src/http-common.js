913f92e57dedd199cc8eb481c0c695bdad7abb1c
truezed <metruezed@gmail.com>
2022-11-09 13:41:45 +0300

    delete .env. fixes.

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 
 
 export default axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL,
+    baseURL: process.env.REACT_APP_BLOTTERY_API,
     headers: {
         "Content-Type": "application/json",
         "Authorization": Cookies.get('token') ? `Token ${Cookies.get('token')}` : null,

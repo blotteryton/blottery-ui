154a89b1ef69771db8d8c61ea6e8e0a792a9b678
truezed <metruezed@gmail.com>
2022-11-09 13:13:37 +0300

    fixes.

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 
 
 export default axios.create({
-    baseURL: process.env.REACT_APP_BLOTTERY_API,
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     headers: {
         "Content-Type": "application/json",
         "Authorization": Cookies.get('token') ? `Token ${Cookies.get('token')}` : null,
